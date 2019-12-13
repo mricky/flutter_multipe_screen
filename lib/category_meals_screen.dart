700cc597622026d6cb3dfcb2ac85908c6df87be9
Mohammad <mricky.it@gmail.com>
2019-12-13 22:38:46 +0700

    Adding a Meal Model & Data

--- lib/category_meals_screen.dart
+++ lib/category_meals_screen.dart
@@ -17,9 +17,10 @@ class CatergoryMealsScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text(categoryTitle),
       ),
-      body: Center(
-        child: Text('The Recipts For The Category'
-       ),
+      body: ListView.builder(
+          itemBuilder: (ctx, index){
+            
+          },
       ),
     );
   }
