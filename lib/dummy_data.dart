ffb32d47e9bd31ec3a914b0e5dac412d2df74ba0
Mohammad <mricky.it@gmail.com>
2019-12-13 14:25:28 +0700

    adding model data

---
+++ lib/dummy_data.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+import './models/category.dart';
+
+const DUMMY_CATEGORIES = const [
+  Category(
+    id: 'c1',
+    title: 'Italian',
+    color: Colors.purple,
+  ),
+   Category(
+    id: 'c2',
+    title: 'Quick & Easy',
+    color: Colors.purple,
+  ),
+   Category(
+    id: 'c3',
+    title: 'Hamburgers',
+    color: Colors.purple,
+  ),
+]
